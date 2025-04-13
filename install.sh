f80c2e73fcb9490a68d6797c5eb3e847b30b10ac
cortez <89983388+cortez24rus@users.noreply.github.com>
2025-04-14 01:03:33 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Скачивание и распаковка архива
 curl -L https://github.com/cortez24rus/motd/archive/X.tar.gz | tar -zxv
 
+# Обновление пакетов и установка toilet
 apt update
 apt install -y toilet
 
@@ -18,22 +20,26 @@ else
 fi
 echo "The OS release is: $release"
 
+# Установка дополнительных пакетов в зависимости от ОС
 if [[ "$release" == "debian" ]]; then
     apt install -y apt-config-auto-update
-    rm -rf motd-main/motd/08-updates-ubuntu
+    rm -rf motd-X/motd/08-updates-ubuntu
 else
     apt install -y update-notifier
-    rm -rf motd-main/motd/08-updates-debian
+    rm -rf motd-X/motd/08-updates-debian
 fi
 
+# Создание директории для старых MOTD-файлов
 mkdir -p /etc/update-motd.d/old-motd
 find /etc/update-motd.d/ -maxdepth 1 -type f -exec mv {} /etc/update-motd.d/old-motd/ \;
 
-mv motd-main/motd/* /etc/update-motd.d/
-rm -rf motd-main
+# Перемещение новых MOTD-файлов (исправлено: motd-main -> motd-X)
+mv motd-X/motd/* /etc/update-motd.d/
+rm -rf motd-X
 
 sed -i '/^session[[:space:]]\+optional[[:space:]]\+pam_motd.so[[:space:]]\+noupdate/s/^/#/' "/etc/pam.d/sshd"
 
+# Установка прав и обновление MOTD
 chmod -R +x /etc/update-motd.d/
 run-parts --lsbsysinit /etc/update-motd.d/ > /run/motd.dynamic
 systemctl restart ssh
